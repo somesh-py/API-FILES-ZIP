b2cfeb6e9cb409107797d8b05d5fc55439c81371
somesh-py <someshsharma615@gmail.com>
2023-07-27 10:41:00 +0530

    create zip formate view for download files in zip formate

--- PDFAPI/app/urls.py
+++ PDFAPI/app/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path('upload-file/',views.upload_files),
     path('upload-files-data/',views.upload_files_data),
     path('download/<int:id>/',views.download),
+    path('download_files/<int:user_id>/', views.download_files_as_zip, name='profile_photo_as_zip'),
+    path('download_files/<int:user_id>/', views.download_files_as_zip, name='aadhar_card_as_zip'),
+    path('download_files/<int:user_id>/', views.download_files_as_zip, name='pan_card_as_zip'),
+    path('download_files/<int:user_id>/', views.download_files_as_zip, name='voter_id_as_zip'),
+    path('download_files/<int:user_id>/', views.download_files_as_zip, name='marksheet_as_zip'),
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- PDFAPI/app/views.py
+++ PDFAPI/app/views.py
@@ -5,6 +5,10 @@ from rest_framework.views import APIView
 import json
 from django.contrib.auth.hashers import make_password,check_password
 from .models import Registration,User_files
+import zipfile
+from django.http import HttpResponse
+import os
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
@@ -75,3 +79,41 @@ def upload_files_data(request):
 def download(request,id):
     data=User_files.objects.get(userid=id)
     return render(request,'downloadpg.html',{'user':data})
+
+
+def download_files_as_zip(request, user_id):
+    if request.method == 'GET':
+        # Check if the user exists and matches the authenticated user
+
+        user = User_files.objects.get(userid=user_id)
+
+
+
+        # Assuming you have a user model with file fields
+        files_to_download = [
+            user.profile_photo,  # Replace with the file fields in your model (e.g., user.aadhar_card, user.pan_card, etc.)
+            # Add other files here
+        ]
+
+        # Create a ZIP archive in memory
+        memory_file = zipfile.ZipFile('files.zip', 'w')
+        for file_to_download in files_to_download:
+            if file_to_download:
+                memory_file.write(file_to_download.path, file_to_download.name)
+
+        # Close the in-memory ZIP file
+        memory_file.close()
+
+        # Create a response with the ZIP content
+        response = HttpResponse(content_type='application/zip')
+        response['Content-Disposition'] = f'attachment; filename="{user}_files.zip"'
+
+        # Open the created ZIP file and write its content to the response
+        with open('files.zip', 'rb') as f:
+            response.write(f.read())
+
+        # Clean up temporary files
+        memory_file.close()
+        os.remove('files.zip')
+
+        return response
